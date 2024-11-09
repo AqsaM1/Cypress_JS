b3742fea8c71b0c50413d96aac3e32cd435027b2
aqsa.manzoor <aqsa.manzoor@bequant.io>
2024-11-09 15:43:44 +0100

    First Commit

---
+++ cypress/e2e/tests/verifyLogin.spec.cy.js
@@ -0,0 +1,40 @@
+import loginPage from "../../pageobjects/loginPage"
+
+
+let userDetails;
+describe('login Test', ()=>{
+
+beforeEach("load fixture",function() { 
+    cy.fixture("userDetails").then((data)=>{
+        userDetails=data
+    })
+    // Visit URL 
+    cy.visit('https://demo.guru99.com/test/newtours/login.php')
+
+
+})
+
+it('Verify Login Successful',()=>{
+
+    const loginObj = new loginPage();
+    loginObj.enterUsername(userDetails.validUserDetails.username);
+    loginObj.enterPassword(userDetails.validUserDetails.password);
+    loginObj.clickSubmit();
+    loginObj.elements.successTxt().should('have.text','Login Successfully')
+
+
+})
+
+it('Verify Login unsuccessful for Invalid username/passwrod ',()=>{
+
+    const loginObj = new loginPage();
+    loginObj.enterUsername(userDetails.invalidUserDetails.username);
+    loginObj.enterPassword(userDetails.invalidUserDetails.password);
+    loginObj.clickSubmit();
+    loginObj.elements.errorTxt().should('contain','Enter your userName an')
+
+})
+
+
+
+})
